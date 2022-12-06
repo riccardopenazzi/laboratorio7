9dd265c2b29a18edbc2577a08d20965abf32d24f
riccardopenazzi <penazziriccardo17@gmail.com>
2022-12-06 18:52:17 +0100

    Esercizio 62 completato, aggiunta casella di testo che riporta lo stesso numero stampato sullo standard output

--- 62-simple-gui/src/main/java/it/unibo/mvc/MiniGUI.java
+++ 62-simple-gui/src/main/java/it/unibo/mvc/MiniGUI.java
@@ -1,8 +1,11 @@
 package it.unibo.mvc;
 
+import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
+
 import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.Toolkit;
@@ -29,19 +32,27 @@ public class MiniGUI {
     public MiniGUI() {
         final JPanel canvas = new JPanel();
         canvas.setLayout(new BorderLayout());
+        final JPanel myCanvas = new JPanel();
+        myCanvas.setLayout(new BoxLayout(myCanvas, BoxLayout.X_AXIS));
+        canvas.add(myCanvas, BorderLayout.CENTER);
         final JButton write = new JButton("Print a random number on standard output");
-        canvas.add(write, BorderLayout.CENTER);
+        myCanvas.add(write);
         frame.setContentPane(canvas);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        final JTextField textField = new JTextField("Prova");
+        canvas.add(textField, BorderLayout.NORTH);
         /*
          * Handlers
          */
         write.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(final ActionEvent e) {
-                System.out.println(randomGenerator.nextInt());
+                int n = randomGenerator.nextInt();
+                System.out.println(n);
+                textField.setText(Integer.toString(n));
             }
         });
+        
     }
 
     private void display() {
